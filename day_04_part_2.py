601643dc747dc7f8bc5d0b7231857b56766d40b0
Julle <jenni.janeskallio@gmail.com>
2020-12-05 12:50:50 +0200

    day 4 finished

--- day_04_part_2.py
+++ day_04_part_2.py
@@ -1,4 +1,4 @@
-with open('temp_day_04_input.txt') as f:
+with open('day_04_input.txt') as f:
 #    passports = [line.rstrip() for line in f]
     passports = f.readlines()
 
@@ -34,10 +34,12 @@ while i < len(passports):
             byr += passport[byr_index]
             byr_index += 1
         
-        if( len(byr) != 4 or (int(byr) < 1920 or 2020 <= int(byr))):
+        if( len(byr) != 4 or (int(byr) < 1920 or 2002 < int(byr))):
             passport_valid = False
+#            print("byr is: " + str(byr))
     else:
         passport_valid = False
+ #       print("no byr")
 
 
     if( iyr_index != -1 and passport_valid ):        
@@ -50,8 +52,10 @@ while i < len(passports):
         
         if( len(iyr) != 4 or (int(iyr) < 2010 or 2020 < int(iyr))):
             passport_valid = False
+#            print("iyr is: " + str(iyr))
     else:
         passport_valid = False
+#        print("no iyr")
 
     if( eyr_index != -1 and passport_valid ):
         eyr = ''
@@ -60,20 +64,25 @@ while i < len(passports):
         while eyr_index < len(passport) and ( passport[eyr_index] != ' ' and passport[eyr_index] != '\n'):
             eyr += passport[eyr_index]
             eyr_index += 1
-        
+
+      
         if( len(eyr) != 4 or (int(eyr) < 2020 or 2030 < int(eyr))):
             passport_valid = False
+#            print("eyr invalid! eyr is: " + str(eyr))
     else:
         passport_valid = False
+#        print("no eyr")
 
     if( hgt_index != -1 and passport_valid ):
         hgt = ''
         hgt_index += 4 
-        
+
         while hgt_index < len(passport) and ( passport[hgt_index] != ' ' and passport[hgt_index] != '\n'):
             hgt += passport[hgt_index]
             hgt_index += 1
         
+#        print("height check starting with: " + hgt)        
+
         if( hgt[-2:] == 'in' or hgt[-2:] == 'cm' ):
             j=0
             height = ''
@@ -81,12 +90,16 @@ while i < len(passports):
                 height += hgt[j]
                 j += 1
             
+#            print("height is: " + height)
+
             if( (hgt[-2:] == 'in' and (int(height) < 59 or int(height) > 76 ))):
                 passport_valid = False
-            elif(hgt[-2:] == 'cm' and (int(height) < 150 or int(height) > 190) ):
+            elif(hgt[-2:] == 'cm' and (int(height) < 150 or int(height) > 193) ):
                 passport_valid = False
         else:
             passport_valid = False
+
+ #       print("this height wast: " + str(passport_valid))
     else:
         passport_valid = False
 
@@ -100,6 +113,7 @@ while i < len(passports):
 
         if( len(hcl) != 7 or hcl[0] != '#'):
             passport_valid = False
+  #          print("hair colour not valid! Hair colour is: " + str(hcl))
         
         if( passport_valid ):
             index_of_char = 1
@@ -111,28 +125,60 @@ while i < len(passports):
                 checklist[number] = str(number)
                 number +=1
 
-            while index_of_char < len(hcl):
+            while index_of_char < len(hcl) and passport_valid:
                 if( (hcl[index_of_char] not in ['a', 'b', 'c', 'd', 'e','f']) and hcl[index_of_char] not in checklist):
                     passport_valid = False
+#                    print("hair colour not valid! Hair colour is: " + str(hcl))
                     break
                 index_of_char += 1
     else:
         passport_valid = False
+ #       print("No hair colour")
 
     if( ecl_index != -1 and passport_valid ):
-        hcl = ''
-        hcl_index += 4 
+        ecl = ''
+        ecl_index += 4 
         
-        while hcl_index < len(passport) and ( passport[hcl_index] != ' ' and passport[hcl_index] != '\n'):
-            hcl += passport[hcl_index]
-            hcl_index += 1
+        while ecl_index < len(passport) and ( passport[ecl_index] != ' ' and passport[ecl_index] != '\n'):
+            ecl += passport[ecl_index]
+            ecl_index += 1
 
-#    if( pid_index != -1 and passport_valid ):
-#    else:
-#        passport_valid = False
+        checklist = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+        if( ecl not in checklist):
+            passport_valid = False
+ #           print("Eye colour not valid! Eye colour is: " + str(ecl))
+    else:
+        passport_valid = False
+        
+    if( pid_index != -1 and passport_valid ):
+        pid = ''
+        pid_index += 4 
+        
+        while pid_index < len(passport) and ( passport[pid_index] != ' ' and passport[pid_index] != '\n'):
+            pid += passport[pid_index]
+            pid_index += 1
+
+        if( len(pid) != 9):
+            passport_valid = False
+    
+        checklist = [None] * 10 
+        number = 0
+        while number < 10:
+            checklist[number] = str(number)
+            number +=1
+
+        index_of_char = 0
+        while index_of_char < len(hcl) and passport_valid:
+            if( pid[index_of_char] not in checklist):
+                passport_valid = False
+                break
+            index_of_char += 1
+
+    else:
+        passport_valid = False
 
     if( passport_valid ):
-        
         no_of_valid_passports += 1
     
     i += 1
