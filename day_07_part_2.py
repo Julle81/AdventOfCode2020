e881982481ecbd78c5b934856d15a3542f34d4af
Julle <jenni.janeskallio@gmail.com>
2020-12-12 20:12:00 +0200

    to work on another comp

--- day_07_part_2.py
+++ day_07_part_2.py
@@ -1,10 +1,12 @@
+import re
+
 with open('day_07_input.txt') as f:
     colour_rules = [line.rstrip() for line in f]
 
 i = 0
 colours_can_contain = []
 temp_colours = []
-amount = 0
+amount_of_bags = 0
 
 while i < len(colour_rules):
     colour_rule = colour_rules[i]
@@ -28,52 +30,17 @@ while i < len(colour_rules):
     colour_rule = colour_rule[len(colour) + 1:]
     if( colour == "shiny gold" ):
         index = colour_rule.find("contain") + len("contain") + 1
-        while colour_rule[index] != " ":
-            string_amount += colour_rule[index]
-            index += 1
 
-        amount += int(string_amount)
+        no_of_bags = [int(s) for s in re.findall(r'\b\d+\b', colour_rule)]
+        bag_index = 0
+        while bag_index < len(no_of_bags):
+            amount_of_bags += no_of_bags[bag_index]
+            bag_index += 1
+        next_colours_to_check = [s for s in re.findall(r'\b[0-9] * bags+\b', colour_rule)]
 
-        temp_colours.append(colour_not_this)
+        temp_colours.append(next_colours_to_check)
     
     i += 1
 
-while True:
-    colours_to_check = []
-    j = 0
-    while j < len(temp_colours):
-        rule_index = 0
-        colour_to_find = temp_colours[j];
-        while rule_index < len(colour_rules):
-            colour_rule = colour_rules[rule_index]
-            colour = ""
-                           
-            pass_through = 0
-            char_index = 0
-            while pass_through < 2:
-                
-                while colour_rule[char_index] != " ":
-                    colour += colour_rule[char_index]
-                    char_index += 1
 
-                if( pass_through == 0):
-                    colour += " "
-                char_index += 1
-                pass_through +=1
-
-            colour_rule = colour_rule[len(colour) + 1:]
-            if( colour_rule.find(colour_to_find) != -1):
-                if colour not in colours_can_contain:
-                    colours_can_contain.append(colour)
-                colours_to_check.append(colour)
-            
-            rule_index += 1
-        j += 1
-
-    temp_colours = colours_to_check
-
-    if not temp_colours:
-        break
-
-
-print(len(colours_can_contain))
+print(temp_colours)
